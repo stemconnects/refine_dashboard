8d38dcb8c8ff536f66ed89e7a9c88ddeac583929
G-McCarten <76708068+G-McCarten@users.noreply.github.com>
2023-03-05 14:38:51 -0500

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import yariga from './yariga.svg';
+import stemconnects from './sc.png'
+import stemconnects_logo from './sc_logo.png'
+
+export {
+  logo,
+  yariga,
+  stemconnects,
+  stemconnects_logo
+};
